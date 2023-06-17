19e7acf3c04ac0526f3163c7e1dfc1fd2cb01dfa
Mitacho <arthurflavionz@gmail.com>
2023-06-17 11:35:03 -0300

    Controllers & Testes E2E - Testes E2E de rotas de check-ins

---
+++ src/http/controllers/check-ins/create.spec.ts
@@ -0,0 +1,37 @@
+import { app } from "@/app";
+import { prisma } from "@/lib/prisma";
+import { createAndAuthenticateUser } from "@/utils/test/create-and-authenticate-user";
+import request from "supertest";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+
+describe("Create check-in (e2e)", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should be able to create a check-in", async () => {
+    const { token } = await createAndAuthenticateUser(app);
+
+    const gym = await prisma.gym.create({
+      data: {
+        title: "JavaScript Gym",
+        latitude: -22.7661764,
+        longitude: -47.3285686,
+      },
+    });
+
+    const response = await request(app.server)
+      .post(`/gyms/${gym.id}/check-ins`)
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        latitude: -22.7661764,
+        longitude: -47.3285686,
+      });
+
+    expect(response.statusCode).toEqual(201);
+  });
+});
